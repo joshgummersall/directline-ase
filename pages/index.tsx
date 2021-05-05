74ba692b1f5428b77f660fe231353c37f97e8c52
Josh Gummersall <jgummersall@microsoft.com>
2021-05-05 13:01:15 -0700

    Add debugger

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import * as rt from "runtypes";
 import Head from "next/head";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import type { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { DirectLineStreaming } from "botframework-directlinejs";
+
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Modal from "react-bootstrap/Modal";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
 
 const Token = rt.Record({
   token: rt.String,
@@ -15,7 +23,7 @@ export const getServerSideProps: GetServerSideProps<{
 }> = async () => {
   try {
     const resp = await fetch(
-      `${process.env.BOT_URL}/.bot/v3/directline/tokens/generate`,
+      `${process.env.NEXT_PUBLIC_BOT_URL}/.bot/v3/directline/tokens/generate`,
       {
         method: "POST",
         headers: {
@@ -40,24 +48,44 @@ const ReactWebChat = dynamic<any>(() => import("botframework-webchat"), {
   ssr: false,
 });
 
+const Status = rt.Record({
+  v: rt.String,
+  k: rt.Boolean,
+  ib: rt.Boolean,
+  ob: rt.Boolean,
+  initialized: rt.Boolean,
+});
+
 export default function Index(
   props: InferGetServerSidePropsType<typeof getServerSideProps>
 ) {
   const { token } = props;
 
-  const [directLine, setDirectLine] = useState(null);
+  const [status, setStatus] = useState<rt.Static<typeof Status> | undefined>();
+
+  const onHide = useCallback(() => setStatus(undefined), [setStatus]);
+
+  const onShow = useCallback(async () => {
+    const resp = await fetch(`${process.env.NEXT_PUBLIC_BOT_URL}/.bot`);
+    if (resp.ok) {
+      setStatus(Status.check(await resp.json()));
+    }
+  }, [setStatus]);
+
+  const [directLine, setDirectLine] = useState<DirectLineStreaming>();
+
   useEffect(() => {
     if (token) {
       setDirectLine(
         new DirectLineStreaming({
-          domain: `${location.protocol}//${location.hostname}/.bot/v3/directline`,
+          domain: `${process.env.NEXT_PUBLIC_BOT_URL}/.bot/v3/directline`,
           token,
         })
       );
     }
-  }, [setDirectLine]);
+  }, [setDirectLine, token]);
 
-  if (!token) {
+  if (!directLine) {
     return null;
   }
 
@@ -66,7 +94,35 @@ export default function Index(
       <Head>
         <title>Webchat Directline ASE</title>
       </Head>
-      <ReactWebChat directLine={directLine} />;
+      <Navbar bg="light" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand>Webchat Debugger</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link onClick={onShow}>Status</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container className="h-100">
+        <Row className="h-100">
+          <Col xs={12}>
+            <ReactWebChat directLine={directLine} />
+          </Col>
+        </Row>
+      </Container>
+      <Modal show={status != null} onHide={onHide}>
+        <Modal.Header closeButton>
+          <Modal.Title>Status</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <pre>{JSON.stringify(status, null, 2)}</pre>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
